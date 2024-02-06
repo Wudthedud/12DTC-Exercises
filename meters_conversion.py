82e113bd13df9929bb1926286767ffebc4fb9e9e
wudthedud <wud@middleton.school.nz>
2024-02-06 23:49:26 +1300

    Create meters_conversion.py

---
+++ meters_conversion.py
@@ -0,0 +1,5 @@
+m = int(input("Enter a number of meters for conversion: "))
+conversions = [[1000, "millimeters"], [100, "centimeters"], [0.001, "kilometers"], [39.3701, "inches"], [3.28084, "feet"], [0.000621371, "miles"], [1.09361, "yards"]]
+print(f"{m} meters is: ")
+for i in conversions:
+    print(f"{m * i[0]} {i[1]}")
